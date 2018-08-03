cd792301e1522e852e0ec83e4e7bd7037f31bf78
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-03 19:34:02 +0200

    Making first component

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="cardBrand">
+          <p> {this.props.brand}</p>
+        </div>
+        <div className="cardModel">
+          <p> {this.props.model} </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
