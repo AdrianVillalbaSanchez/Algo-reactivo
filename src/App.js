2120afd53fe7be4a6a36635d04c54c55f66de9c0
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-04 18:06:34 +0200

    hover added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <Card brand={"Gibson"} model={"Les Paul"} year={"1959"} />
+        <Card brand={"Fender"} model={"Stratocaster"} year={"1964"} />
       </div>
     );
   }
