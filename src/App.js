9bddbc719e67f9be026de8ff3741b3677bf27789
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-17 18:56:42 +0200

    Form added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import Link from './Link';
 
-
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      link: ''
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
+
+        <div className="formIdea">
+          <form>
+            <label>
+              Title:
+            </label>
+            <input type="text" name="title" />
+            <br />
+            <label>
+              Link:
+            </label>
+            <input type="text" name="link" />
+            <br />
+            <button type="submit"> Add new link </button>
+          </form>
+
+        </div>
+
         <div><Link title={"Title"} link={"Link"} /> </div>
-      </div>
+
+      </div >
     );
   }
 }
