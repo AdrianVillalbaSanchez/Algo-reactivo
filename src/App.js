da45fcf48f40ac28cf2e0475170c91a794ec3e37
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-17 17:52:30 +0200

    Thinking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Card from './Card';
+import Link from './Link';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Card brand={"Gibson"} model={"Les Paul"} year={"1959"} />
-        <Card brand={"Fender"} model={"Stratocaster"} year={"1964"} />
+        <div><Link brand={"Gibson"} model={"Les Paul"} year={"1959"} /> </div>
       </div>
     );
   }
---
+++ src/Link.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Link extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="webTitle">
+          <p> {this.props.title}</p>
+        </div>
+        <div className="webLink">
+          <p> {this.props.link} </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Link;
